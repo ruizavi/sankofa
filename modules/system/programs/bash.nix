27192e8a53a8de35174bd7be390145d30618fbd1
aviruiz <avi.ruiz@proton.me>
2024-05-10 03:13:14 +0000

    /bin/bash

--- modules/system/programs/bash.nix
+++ modules/system/programs/bash.nix
@@ -3,6 +3,13 @@
   pkgs,
   ...
 }: {
+  system.activationScripts.binbash = {
+    deps = ["binsh"];
+    text = ''
+      ln -s /bin/sh /bin/bash
+    '';
+  };
+
   programs.bash = {
     promptInit = ''eval "$(${lib.getExe pkgs.starship} init bash)"'';
   };
